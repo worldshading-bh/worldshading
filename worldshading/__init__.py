2f39e73983cb54c428e514799a14db237731fbbe
Hilal Habeeb <it.development@worldshading.com>
2025-04-15 11:44:24 +0000

    removed monkey patch- back to scratch

--- worldshading/__init__.py
+++ worldshading/__init__.py
@@ -1,15 +1,4 @@
 from __future__ import unicode_literals
 __version__ = '0.0.1'
 
-# def apply_monkey_patches():
-#     print("🔧 Monkey patch being applied...")
-#     import erpnext.stock.reorder_item as reorder_core
-#     from worldshading.patches import reorder_override
 
-#     reorder_core.create_material_request = reorder_override.create_material_request
-#     reorder_core._reorder_item = reorder_override._reorder_item
-
-#     print("✅ create_material_request and _reorder_item patched!")
-
-
-# apply_monkey_patches()
